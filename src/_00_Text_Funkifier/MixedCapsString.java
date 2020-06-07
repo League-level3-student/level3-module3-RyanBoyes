41a8763277f2a0dc55f6f141c96ea4d08a6992fa
RyanBoyes <ryanboyes04@gmail.com>
2020-06-07 11:17:23 -0700

    fg
    f

--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -10,18 +10,33 @@ public class MixedCapsString extends SpecialString{
 	public String funkifyText(String s) {
 		
 	int length = s.length();
+	String newS = "";
 	
 	for(int i = 0; i<length; i++) {
+		char x = s.charAt(i);
 		if(i%2==1) {
+		
+		char Uppered = Character.toUpperCase(x);
+		
+			System.out.println(Uppered);
 			
+			s = s.replace(x, Character.toUpperCase(x));
+			newS+=Uppered;
+				
 		}
-		else if(i%2==0) {
+		if(i%2==0) {
 			
+		
+		char Lowered = Character.toLowerCase(x);
 			
+			System.out.println(Lowered);
+			
+			s = s.replace(x, Character.toLowerCase(x));
+			newS+=Lowered;
 		}
 	}
 		
-		return s;
+		return newS;
 		
 		
 	}
--- src/_00_Text_Funkifier/_00_TestSpecialString.java
+++ src/_00_Text_Funkifier/_00_TestSpecialString.java
@@ -23,7 +23,7 @@ public class _00_TestSpecialString {
 		assertEquals("tset a si sihT", bs.toString());
 	}
 	
-	//3. Fill in the funkifyTxt method in the MixedCapsString class
+	//3. Fill in the funkifyText method in the MixedCapsString class
 	//   to make the JUnitTest case pass.
 	@Test
 	public void testMixedCapsString() {
@@ -39,5 +39,8 @@ public class _00_TestSpecialString {
 	@Test
 	public void testCustomString() {
 	
+		SpecialString ac = new CustomString("the third test now");
+		System.out.println(ac);
+		assertEquals("THE THIRD TEST NOW", ac.toString());
 	}
 }
